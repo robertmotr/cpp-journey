06e44a60d56db08b0cb1cc60ae3434351557ef93
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-01-10 23:36:36 -0500

    finished problem

--- search.cpp
+++ search.cpp
@@ -1,11 +1,16 @@
-#include <algorithm>
-#include <vector>
-#include <tuple>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
 
 using namespace std;
 
+bool comp(const tuple<int, int, int>& i, const tuple<int, int, int>& j) {
+
+}
+
 int main() {
+
     vector<tuple<int, int, int>> vec;
     vec.emplace_back(2, 3, 4);
     vec.emplace_back(5, 9, 10);
@@ -18,5 +23,10 @@ int main() {
     for(auto i : vec) {
         cout << to_string(get<0>(i)) + ", " + to_string(get<1>(i)) + ", " + to_string(get<2>(i)) << endl;  
     }
+
+    // test comparator
     
+
+
+    return 0;
 }
