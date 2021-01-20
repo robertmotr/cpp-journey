16d91c30d3328b4eac94b53d4c3bc5b3fb853d09
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-01-20 17:17:06 -0500

    updated dummy

--- game-hacking/dummy.cpp
+++ game-hacking/dummy.cpp
@@ -14,12 +14,13 @@ int main() {
     int*** ptr2ptr2 = &ptr2ptr;
     while(true) {
         system("CLS");
-        cout << "Process ID: " << GetCurrentProcessId() << endl;
-        cout << "varInt address: " << &varInt << " varInt value: " << varInt << endl;
-        cout << "arrChar address: " << &arrChar << " arrChar value: " << arrChar << endl;
-        cout << "intptr address: " << &intptr << " intptr value: " << intptr << endl;
-        cout << "ptr2ptr address: " << &ptr2ptr << " ptr2ptr value: " << ptr2ptr << endl;
-        cout << "ptr2ptr2 address: " << &ptr2ptr2 << " ptr2ptr2 value: " << ptr2ptr2 << endl;
+        cout << "Process ID: " << dec << GetCurrentProcessId() << endl;
+        cout << "varInt address: " << hex << &varInt << endl;
+        cout << "varInt value: " << dec << varInt << endl;
+        cout << "arrChar address: " << hex << &arrChar << " arrChar value: " << arrChar << endl;
+        cout << "intptr address: " << hex << &intptr << " intptr value: " << intptr << endl;
+        cout << "ptr2ptr address: " << hex << &ptr2ptr << " ptr2ptr value: " << ptr2ptr << endl;
+        cout << "ptr2ptr2 address: " << hex << &ptr2ptr2 << " ptr2ptr2 value: " << ptr2ptr2 << endl;
         cout << "Press SPACE to refresh" << endl;
         getchar();
     }
