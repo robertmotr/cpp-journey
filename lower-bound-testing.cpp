eb3add459d9e2ef42a7f51f6a6f3ffdc60b094ec
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-02-13 01:14:56 -0500

    Update lower-bound-testing.cpp

--- lower-bound-testing.cpp
+++ lower-bound-testing.cpp
@@ -10,12 +10,16 @@ bool compsortf(const vector<int>& a, const vector<int>& b) {
     return a[0] < b[0];
 }
 
-bool complowf(const vector<int>& a, int val) {
+bool complowf(const vector<int>& a, int val)  {
     return a[0] < val;
 }
 
+bool comphighf(int val, const vector<int>& a) {
+    return a[0] > val;
+}
+
 int main() {
-    vector<vector<int>> vec = {{0, 6, 9}, {2, 3, 5}, {1, 4, 8}, {6, 8, 3}, {4, 2 ,1}, {9, 9, 9}};
+    vector<vector<int>> vec = {{0, 6, 9}, {2, 3, 5}, {2, 9, 1},{1, 4, 8}, {6, 8, 3}, {4, 2 ,1}, {9, 9, 9}};
     sort(vec.begin(), vec.end(), compsortf);
 
     for(int i = 0; i < vec.size(); i++) {
@@ -26,8 +30,12 @@ int main() {
     cout << "" << endl;
     }
 
-    vector<vector<int>>::iterator it = lower_bound(vec.begin(), vec.end(), 9, complowf);
+    vector<vector<int>>::iterator it = lower_bound(vec.begin(), vec.end(), 2, complowf);
+    vector<vector<int>>::iterator upit = upper_bound(vec.begin(), vec.end(), 2, comphighf);
     cout << "iterator:" << endl;
-    cout << it - vec.begin() << endl;
+    cout << distance(vec.begin(), it) << endl;
+
+    cout << "iterator:" << endl;
+    cout << distance(vec.begin(), upit) << endl;
     return 0;
 }
