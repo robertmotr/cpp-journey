ac632fd24f8fc00e99bfa36c9970e3e43057aff1
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-02-16 00:18:54 -0500

    Update S32020.cpp

--- waterloo/S32020.cpp
+++ waterloo/S32020.cpp
@@ -7,16 +7,49 @@
 using namespace std;
 
 int main() {
-    string ln;
+    string haystack;
+    getline(cin, haystack);
+    string needle = haystack;
+    getline(cin, haystack);
 
-    getline(cin, ln);
+    set<string> set;
+    map<char, int> needlemap;
+    bool b = true;
 
-    string orig = ln;
-    getline(cin, ln);
+    cout << needlemap['a'] << endl;
 
-    int i = 0;
-    while(i < ln.length()) {
-        
-    }
+    // for(int k = 0; k < needle.size(); k++) {
+    //     needlemap[needle[k]] += 1;
+    // }
+    // for(int i = 0; i < haystack.size() - needle.size();) {
+    //     map<char, int> haystackmap;
+    //     int j = i;
+    //     string ln = "";
+    //     if(b) {
+    //         for(int m = i; m < needle.size() + i; m++) {
+    //             ln += haystack[m];
+    //             haystackmap[haystack[m]] += 1;
+    //         }
+    //     }
+    //     if(haystackmap == needlemap) {
+    //         haystackmap.clear();
+    //         i += needle.size();
+    //         set.insert(ln);
+    //         b = true;
+    //     }
+    //     else {
+    //         b = false;
+    //         i += 1;
+    //         if(needlemap[haystack[j]] != 0) {
+    //             haystackmap[haystack[j]] -= 1;
+    //         }
+    //         else {
+    //             haystackmap.erase(haystack[j]);
+    //         }
+    //         haystackmap[i + needle.size()] += 1;
+    //     }
+    // }
+
+    // cout << set.size() << endl;
     return 0;
 }
