ff77a5b07dab2472c377553709c84aa6652dba72
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-02-16 00:19:06 -0500

    Update S32020.cpp

--- waterloo/S32020.cpp
+++ waterloo/S32020.cpp
@@ -18,38 +18,38 @@ int main() {
 
     cout << needlemap['a'] << endl;
 
-    // for(int k = 0; k < needle.size(); k++) {
-    //     needlemap[needle[k]] += 1;
-    // }
-    // for(int i = 0; i < haystack.size() - needle.size();) {
-    //     map<char, int> haystackmap;
-    //     int j = i;
-    //     string ln = "";
-    //     if(b) {
-    //         for(int m = i; m < needle.size() + i; m++) {
-    //             ln += haystack[m];
-    //             haystackmap[haystack[m]] += 1;
-    //         }
-    //     }
-    //     if(haystackmap == needlemap) {
-    //         haystackmap.clear();
-    //         i += needle.size();
-    //         set.insert(ln);
-    //         b = true;
-    //     }
-    //     else {
-    //         b = false;
-    //         i += 1;
-    //         if(needlemap[haystack[j]] != 0) {
-    //             haystackmap[haystack[j]] -= 1;
-    //         }
-    //         else {
-    //             haystackmap.erase(haystack[j]);
-    //         }
-    //         haystackmap[i + needle.size()] += 1;
-    //     }
-    // }
+    for(int k = 0; k < needle.size(); k++) {
+        needlemap[needle[k]] += 1;
+    }
+    for(int i = 0; i < haystack.size() - needle.size();) {
+        map<char, int> haystackmap;
+        int j = i;
+        string ln = "";
+        if(b) {
+            for(int m = i; m < needle.size() + i; m++) {
+                ln += haystack[m];
+                haystackmap[haystack[m]] += 1;
+            }
+        }
+        if(haystackmap == needlemap) {
+            haystackmap.clear();
+            i += needle.size();
+            set.insert(ln);
+            b = true;
+        }
+        else {
+            b = false;
+            i += 1;
+            if(needlemap[haystack[j]] != 0) {
+                haystackmap[haystack[j]] -= 1;
+            }
+            else {
+                haystackmap.erase(haystack[j]);
+            }
+            haystackmap[i + needle.size()] += 1;
+        }
+    }
 
-    // cout << set.size() << endl;
+    cout << set.size() << endl;
     return 0;
 }
