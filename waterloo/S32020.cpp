b63e3b6c4edef8d482397a85a994431cd90bec73
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-02-15 23:23:28 -0500

    Update S32020.cpp

--- waterloo/S32020.cpp
+++ waterloo/S32020.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <string>
+#include <set>
+#include <map>
 
 using namespace std;
 
 int main() {
+    string ln;
+
+    getline(cin, ln);
+
+    string orig = ln;
+    getline(cin, ln);
+
+    int i = 0;
+    while(i < ln.length()) {
+        
+    }
     return 0;
 }
