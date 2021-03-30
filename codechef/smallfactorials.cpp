14360d9b5ee37bf3c5c5e70e65cf04dd7c217af7
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-03-29 22:15:21 -0400

    finished problem

---
+++ codechef/smallfactorials.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace boost::multiprecision;
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        int a;
+        cin >> a;
+        int fact = 1;
+        while(a != 1) {
+            fact = fact * a;
+            a--;
+        }
+        cout << fact << "\n";
+    }
+    return 0;
+}
