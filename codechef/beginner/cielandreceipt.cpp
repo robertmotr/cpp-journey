9338ff254a2c787fe2e5f023289afc78e3c6c1c8
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-04-03 01:48:25 -0400

    Update cielandreceipt.cpp

--- codechef/beginner/cielandreceipt.cpp
+++ codechef/beginner/cielandreceipt.cpp
@@ -2,24 +2,31 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int compute[12] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
     int t;
     cin >> t;
-    for(int k = 0; k < t; k++) {
+    for (int k = 0; k < t; k++)
+    {
         int p;
         cin >> p;
         int counter = 0;
-        for(int i = 11; i >= 0; i--) {
-            if(p >= compute[i]) {
-                p -= compute[i];
-                counter += 1;
-                break;
+        while (p != 0)
+        {
+            for (int i = 11; i >= 0; i--)
+            {
+                if (p >= compute[i])
+                {
+                    p -= compute[i];
+                    counter += 1;
+                    break;
+                }
+            }
         }
-    }
     }
     return 0;
 }
