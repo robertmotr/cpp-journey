1e2871be64c3f3470b503838930150ce6d379321
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-04-23 00:02:00 -0400

    Update led.ino

--- arduino/led/led.ino
+++ arduino/led/led.ino
@@ -10,11 +10,16 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
   
   delay(2000);
   display.clearDisplay();
 
-  display.setTextSize(1);
+  display.setTextSize(5);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
   // Display static text
@@ -23,5 +28,5 @@ void setup() {
 }
 
 void loop() {
-  
+  display.startscrollright(0x00, 0x0F);
 }
