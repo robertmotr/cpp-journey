746a7cb80a4cb927d8149033eb159c1cd35a3f68
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-05-02 00:33:19 -0400

    fixed it

--- ecoo/real/dnaderren.cpp
+++ ecoo/real/dnaderren.cpp
@@ -9,10 +9,29 @@ int main() {
     cin.tie(NULL);
 
     string in;
-    cin >> in;
+    getline(cin, in);
 
-    
+    for(int i = 0; i < in.size() - 1;) {
+        if(in[i] == 'A') {
+            if(in[i + 1] == 'C' || in[i + 1] == 'T' || in[i + 1] == 'G') {
+                i++;
+            }
+            else {
+                in.insert(i + 1, 1, ' ');
+                i += 2;
+            }
+        }
+        else if(in[i] == 'C' || in[i] == 'T' || in[i] == 'G') {
+            if(in[i + 1] == 'A') {
+                i++;
+            }
+            else {
+                in.insert(i + 1, 1, ' ');
+                i += 2;
+            }
+        }
+    }
 
-    cout << out << endl;
+    cout << in << endl;
     return 0;
 }
