a335d761596e7540b1ded57b0eafaad0c84ebe3b
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2022-07-31 01:41:51 -0400

    push

--- leetcode/romantointeger.cpp
+++ leetcode/romantointeger.cpp
@@ -1,22 +1,60 @@
 #include <iostream>
 #include <string>
-#include <map>
 
 using namespace std;
 
 class Solution {
 public:
-    int romanToInt(string s) {
-        map<char, int> numerals = {{'M', 1000}, {'D', 500}, {'C', 100},
-         {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
-        int number = 0;
-        for(char i: s) {
-            number += numerals[i];
-        }
-        return number;
-    }
-};
 
-int main() {
-    return 0;
+    void replaceAll(string& subject, const string& search,
+                          const string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }}
+
+    int romanToInt(string s) {
+                        int num = 0;
+
+                        replaceAll(s, "IV", "IIII");
+                        replaceAll(s, "IX", "VIIII");
+                        replaceAll(s, "XL", "XXXX");
+                        replaceAll(s, "XC", "LXXXX");
+                        replaceAll(s, "CD", "CCCC");
+                        replaceAll(s, "CM", "DCCCC");
+
+                        for(const char& ch : s) {
+                                if(ch == 'I') {
+                                        num += 1;
+                                }
+                                else if(ch == 'V') {
+                                        num += 5;
+                                }
+                                else if(ch == 'X') {
+                                        num += 10;
+                                }
+                                else if(ch == 'L') {
+                                        num += 50;
+                                }
+                                else if(ch == 'C') {
+                                        num += 100;
+                                }
+                                else if(ch == 'D') {
+                                        num += 500;
+                                }
+                                else {
+                                        num += 1000;
+                                }
+                        }
+
+                        return num;
+                }
+
+    };
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+
 }
