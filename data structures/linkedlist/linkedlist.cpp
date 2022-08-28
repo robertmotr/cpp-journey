e658b82eb388590ceb5bb964713ec3ac3582724c
Robert Motrogeanu <robert.motrogeanu@proton.me>
2022-08-28 17:56:24 -0400

    Update linkedlist.cpp

--- data structures/linkedlist/linkedlist.cpp
+++ data structures/linkedlist/linkedlist.cpp
@@ -23,6 +23,19 @@ LinkedList<T>::LinkedList(T value) {
 }
 
 template<typename T>
+LinkedList<T>::~LinkedList() {
+
+    Node<T>* current = this->head;
+    while(current != nullptr) {
+        Node<T>* next = current->next;
+        delete current;
+        current = next;
+    }
+
+    cout << "Destroyed linked list!" << "\n";
+}
+
+template<typename T>
 void LinkedList<T>::append(T value) {
 
     Node<T>* new_node = new Node<T>(value);
@@ -32,15 +45,107 @@ void LinkedList<T>::append(T value) {
         this->tail = new_node;
     }
     else {
-        Node<T>* curr = this->tail;
+        Node<T>* tmp = this->tail;
+        Node<T>* newtail = new Node<T>(value);
 
-        new_node->prev = this->tail;
-        curr->next = new_node;
-        this->tail = 
+        tmp->next = newtail;
+        newtail->prev = tmp;
+
+        this->tail = newtail;
     }
-} 
+
+    this->size += 1;
+}
 
 template<typename T>
-LinkedList<T>::~LinkedList() {
-    cout << "destroyed linked list" << "\n";
+T LinkedList<T>::pop() {
+    T value = this->tail->data;
+    Node<T>* temp = this->tail;
+
+    this->tail = this->tail->prev;
+    this->tail->next = nullptr;
+
+    delete temp;
+
+    this->size -= 1;
+    return value;
 }
+
+template<typename T>
+T LinkedList<T>::pop(T value) {
+    Node<T>* current = this->head;
+    
+    bool found = false;
+    while(current != nullptr) {
+        if(current->data == value) {
+            found = true;
+            break;
+        }
+        current = current->next;
+    }
+
+    if(found == true) {
+        Node<T>* tmp = current;
+        current->next->prev = current->prev;
+        current->prev->next = current->next;
+
+        delete current;
+        return value;
+    }
+    return -1;
+}
+
+template<typename T>
+void LinkedList<T>::insert(int position, T item) {
+    this->size += 1;
+    Node<T>* curr = this->head;
+    int counter = 0;
+    while(curr != nullptr && counter <= position) {
+        curr = curr->next;
+        counter += 1;
+    }
+
+    Node<T>* newnode = new Node<T>(item);
+    newnode->next = curr;
+    newnode->prev = curr->prev;
+    curr->prev->next = newnode;
+    curr->prev = newnode;
+}
+
+template<typename T>
+int LinkedList<T>::search(T value) {
+    Node<T>* curr = this->head;
+    int counter = 0;
+    while(curr != nullptr) {
+        if(curr->data == value) {
+            return counter;
+        }
+        curr = curr->next;
+    }
+    return -1;
+}
+
+template<typename T>
+bool isPresent(T value) {
+    Node<T>* curr = this->head;
+    while(curr != nullptr) {
+        if(curr->data == value) {
+            return true;
+        }
+        curr = curr -> next;
+    }
+    return false;
+}
+
+template<typename T>
+void LinkedList<T>::printAll() {
+    Node<T>* curr = this->head;
+    while(curr != nullptr) {
+        cout << curr->data << "\n";
+        curr = curr->next;
+    }
+}
+
+
+
+
